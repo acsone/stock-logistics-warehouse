63ad6fcfc3e8bc87b7a22be5ef3f7a845010cae9
Thomas Binsfeld <thomas.binsfeld@acsone.eu>
2020-07-16 12:44:49 +0200

    [MIG] packaging_uom from 11.0 to 13.0 (pre-commit)

--- packaging_uom/__manifest__.py
+++ packaging_uom/__manifest__.py
@@ -2,18 +2,13 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Packaging UOM",
-    "version": "11.0.1.0.0",
-    "author": 'ACSONE SA/NV, '
-              'Odoo Community Association (OCA)',
+    "version": "13.0.1.0.0",
+    "author": "ACSONE SA/NV, " "Odoo Community Association (OCA)",
     "category": "Warehouse",
     "website": "http://www.acsone.eu",
-    'summary': "Use uom in package",
-    "depends": [
-        "product",
-    ],
-    "data": [
-        "views/product_packaging_views.xml",
-    ],
+    "summary": "Use uom in package",
+    "depends": ["product"],
+    "data": ["views/product_packaging_views.xml"],
     "license": "AGPL-3",
     "installable": True,
 }
